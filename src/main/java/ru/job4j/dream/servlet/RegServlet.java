2dc1623063eebd28d4a7d0cd9c95942aeda34cde
ainz <ainz713@gmail.com>
2021-08-28 22:20:58 +0300

    4. Регистрация пользователя.

--- src/main/java/ru/job4j/dream/servlet/RegServlet.java
+++ src/main/java/ru/job4j/dream/servlet/RegServlet.java
@@ -11,21 +11,20 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-public class AuthServlet extends HttpServlet {
+public class RegServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Store store = PsqlStore.instOf();
+        String name = req.getParameter("name");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
-        User admin = PsqlStore.instOf().findUserByEmail(email);
-        if (admin != null
-                && admin.getPassword().equals(password)) {
-            HttpSession sc = req.getSession();
-            sc.setAttribute("user", admin);
-            resp.sendRedirect(req.getContextPath() + "/posts.do");
+        User user = new User(0, name, email, password);
+        if (store.findUserByEmail(email) == null) {
+            store.save(user);
+            resp.sendRedirect(req.getContextPath() + "/candidates.do");
         } else {
-            req.setAttribute("error", "Не верный email или пароль");
-            req.getRequestDispatcher("login.jsp").forward(req, resp);
+            req.setAttribute("error", "Пользователь существует");
+            req.getRequestDispatcher("reg.jsp").forward(req, resp);
         }
     }
 }
-
--- src/main/java/ru/job4j/dream/store/PsqlMain.java
+++ src/main/java/ru/job4j/dream/store/PsqlMain.java
@@ -11,8 +11,8 @@ public class PsqlMain {
         store.save(new Candidate(0, "Can1"));
         store.save(new Candidate(1, "Can2"));
         store.save(new Candidate(0, "Can3"));
-        store.save(new User(0, "User2", "user1@mail.ru", "123"));
-        store.save(new User(1, "User25858", "user2@mail.ru", "123"));
+        store.save(new User(0, "123", "123@mail.ru", "123"));
+        //store.save(new User(1, "User25858", "user2@mail.ru", "123"));
         for (User user : store.findAllUsers()) {
             System.out.println(user.getId() + " " + user.getName() + " " + user.getEmail() + " " + user.getPassword());
         }
